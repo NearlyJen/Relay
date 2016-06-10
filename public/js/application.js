c36c3a6b27acebff07cd83b86716ae491ec39853
Theron Cross <theroncross@gmail.com>
2016-06-10 09:37:07 -0500

    OOJS the timer

--- public/js/application.js
+++ public/js/application.js
@@ -4,45 +4,55 @@ $(document).ready(function() {
     $(this).closest('div').find('.elapsed_time').text(tenths);
   })
 
-  var h1 = document.getElementsByTagName('h1')[0],
-      start = document.getElementById('start'),
-      stop = document.getElementById('stop'),
-      clear = document.getElementById('clear'),
-      tenths = 0, seconds = 0, minutes = 0,
-      t;
+  var $readout = $('#timer_readout');
+  var timer = new Timer($readout);
 
-  function add() {
-    tenths++;
-    if (tenths % 10 === 0) {
-      seconds++;
-      if (seconds >= 60) {
-        seconds = 0;
-        minutes++;
-      }
-    }
-    formatTime();
-    timer();
-  }
+  $('#start').on('click', function() { timer.start() });
+  $('#stop' ).on('click', function() { timer.stop()  });
+  $('#clear').on('click', function() { timer.clear() });
 
-  function formatTime() {
-    h1.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds) + "." + (tenths % 10)
-  }
-
-  function timer() {
-    t = setTimeout(add, 100);
-  }
-
-  /* Start button */
-  start.onclick = timer;
-
-  /* Stop button */
-  stop.onclick = function() {
-    clearTimeout(t);
-  }
-
-  /* Clear button */
-  clear.onclick = function() {
-    h1.textContent = "00:00.0";
-    tenths = 0, seconds = 0, minutes = 0;
-  }
 });
+
+
+function Timer(display) {
+  this.display    = display;
+  this.tenths     = 0;
+  this.seconds    = 0;
+  this.minutes    = 0;
+  this.inervalId  = 0;
+}
+
+Timer.prototype.incrementTimer = function() {
+  this.tenths++;
+  // console.log(this.tenths);
+  console.log(this.seconds);
+
+  if (this.tenths % 10 === 0) {
+    this.seconds++;
+  }
+
+  if (this.seconds >= 60) {
+    this.seconds = 0;
+    this.minutes++;
+  }
+
+  this.updateDisplay();
+}
+
+Timer.prototype.updateDisplay = function() {
+  console.log(this.display);
+  this.display.text((this.minutes ? (this.minutes > 9 ? this.minutes : "0" + this.minutes) : "00") + ":" + (this.seconds > 9 ? this.seconds : "0" + this.seconds) + "." + (this.tenths % 10));
+}
+
+Timer.prototype.start = function() {
+  this.inervalId = setInterval(this.incrementTimer.bind(this), 100);
+}
+
+Timer.prototype.stop = function() {
+  clearInterval(this.inervalId);
+}
+
+Timer.prototype.clear = function() {
+  this.display.text("00:00.0");
+  this.tenths = 0, this.seconds = 0, this.minutes = 0;
+}
